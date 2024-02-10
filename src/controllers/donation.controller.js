016e25ed26fca10270f1b85c578facdd1a60ffd7
Leonardo Tolaba <leonardotolaba.20@gmail.com>
2024-02-09 21:18:58 -0300

    Actualiza las credenciales de acceso de MercadoPago y las URL de redireccionamiento

--- src/controllers/donation.controller.js
+++ src/controllers/donation.controller.js
@@ -3,43 +3,42 @@ import { MercadoPagoConfig, Payment, Preference } from "mercadopago";
 export const createDonation = async (req, res) => {
   const client = new MercadoPagoConfig({
     accessToken:
-      "TEST-7313224845884334-012219-5efbfd9b36d05a9ae6782bfc426e2421-52184901",
+      "TEST-2269610763553663-012416-74611a1b8dfe808e1713d573b6bdb3ea-1651477105",
   });
 
   try {
     const body = {
       items: [
         {
-          title: req.body.title.toString(),
+          title: 'Donación a Por Ellos Ezeiza',
           quantity: Number(req.body.quantity),
           unit_price: Number(req.body.price),
           currency_id: "ARS",
         },
       ],
       back_urls: {
-        success: "https://porellosezeiza.vercel.app/",
+        success: "http://localhost:5000/success",
         pending: "http://localhost:5000/pending",
-        failure: "https://porellosezeiza.vercel.app/donations",
+        failure: "http://localhost:5000/failure",
       },
-      notification_url: "https://pawsofhope.4.us-1.fl0.io/webhook",
+      notification_url: "https://3260-2800-810-50d-8632-554d-9aec-7e79-ab69.ngrok-free.app/webhook",
       auto_return: "approved",
     };
     const preference = new Preference(client);
     const result = await preference.create({body});
     
     res.json({
-      id:result.id,
-      url:result.sandbox_init_point
+      url:result.init_point
     })
   } catch (error) {
     console.log('Error en el catch:', error);
-    res.status(500).json({
+    res.status(501).json({
       message: "Error al crear la preferencia",
     });
   }
 };
 
 export const receiveWebhook = (req, res) => {
-  console.log(req);
+  console.log(req.query);
   res.send("webhook received");
 };
