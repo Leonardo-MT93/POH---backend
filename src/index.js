4f5025d6155ff751db1b74bf2cd34725f3ef745a
Leonardo Tolaba <leonardotolaba.20@gmail.com>
2024-02-12 15:47:55 -0300

    Actualizar configuración de CORS y agregar registro de servidor en el puerto especificado

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-import express from 'express';
-import donationRoutes from './routes/donation.js';
-import {PORT} from './config.js';
-import morgan from 'morgan';
-import cors from 'cors';
+import express from "express";
+import donationRoutes from "./routes/donation.js";
+import { PORT } from "./config.js";
+import morgan from "morgan";
+import cors from "cors";
 const app = express();
 app.use(express.json());
-app.use(morgan('dev'));
-app.use(cors(
-    {
-        origin: 'https://porellosezeiza.vercel.app/donations'
-    }
-));
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://porellosezeiza.vercel.app/');
-    next();
-});
-app.use(donationRoutes)
+app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST",
+    preflightContinue: false,
+  })
+);
+
+app.use(donationRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
